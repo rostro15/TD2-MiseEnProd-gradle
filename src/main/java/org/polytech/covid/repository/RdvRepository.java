7b39059391886c14cc1d2cda161c2caffc571288
Lucie <luciegarnier59@gmail.com>
2023-09-29 14:27:37 +0200

    Fix RDV

--- src/main/java/org/polytech/covid/repository/RdvRepository.java
+++ src/main/java/org/polytech/covid/repository/RdvRepository.java
@@ -1,16 +1,9 @@
 package org.polytech.covid.repository;
 
 import org.polytech.covid.domain.Rdv;
-import org.polytech.covid.domain.VaccinationCenter;
-import org.polytech.covid.domain.Patient;
-import org.polytech.covid.domain.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RdvRepository extends JpaRepository<Rdv, Integer> {
     
     public Rdv findByDate(String date);
-    public Rdv findByCenter(VaccinationCenter center);
-    public Rdv findByPatient(Patient patient);
-    public Rdv findByDoctor(Doctor doctor);
-
 }
